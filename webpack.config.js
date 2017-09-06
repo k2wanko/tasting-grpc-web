10050c456d8d2cd034fc3ff622f343df2244005a
Kazuhiro Kubota <k2.wanko@gmail.com>
2017-09-06 11:02:24 +0900

    Setup Vue project

---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('webpack-html-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+var isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: 'build.js?[hash]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          extractCSS: true,
+          preserveWhitespace: false,
+          loaders: {
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = 'none'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    }),
+    new ExtractTextPlugin({
+      filename: 'style.css?[chunkhash]'
+    }),
+  ])
+}
+
+module.exports.plugins = (module.exports.plugins || [
+  new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    inject: 'body',
+    minify: {
+      collapseWhitespace: isProd,
+    }
+  }),
+]).concat()
